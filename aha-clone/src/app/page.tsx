f1d42af8ead90483262a359e64a43df136b07cf7
Pugazh-DTX <pugazhendhi.n@codedtx.com>
2025-04-08 17:40:47 +0530

    minor changes

--- aha-clone/src/app/page.tsx
+++ aha-clone/src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
 
       <div className="header">
       <h1>Welcome To Aha Clone</h1>
+      <p>First step to create small comoonents</p>
       </div>
         
     </div>
