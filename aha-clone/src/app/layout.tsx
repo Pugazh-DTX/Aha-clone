479b36d83c10c288d30d824a849f9ab02b8c37e9
Mahesh Prabhu B <maheshprabhu.b@codedtx.com>
2025-04-10 17:19:14 +0530

    feat: header-section

--- aha-clone/src/app/layout.tsx
+++ aha-clone/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/layout/footer/footer";
+import Header from "@/components/layout/header/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         {children}
         <Footer />
       </body>
