2a11842cefc6749efb2926182c6e5eda3bea73a0
Mahesh Prabhu B <maheshprabhu.b@codedtx.com>
2025-04-11 14:26:07 +0530

    feat: header-responsive-bottom-nav

--- aha-clone/src/components/layout/header/header.tsx
+++ aha-clone/src/components/layout/header/header.tsx
@@ -11,13 +11,18 @@ import logo from "../../../../public/Assets/icons/Logo.svg";
 import search from "../../../../public/Assets/icons/Header/search-icon.svg";
 import avatar from "../../../../public/Assets/icons/Header/avator-icon.svg";
 import { Button } from "../../atoms/button";
-
+import BottomNav from "./BottomNav.tsx";
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const pathname = usePathname();
   const containerRef = useRef<HTMLDivElement>(null);
   const [hasOverflow, setHasOverflow] = useState(false);
+  const [currentPath, setCurrentPath] = useState("");
+
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
 
   useEffect(() => {
     const el = containerRef.current;
@@ -43,111 +48,107 @@ const Header = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <header
-      className={`${styles.header} ${
-        scrolled ? styles.scrolled : styles.atTop
-      }`}
-    >
-      <div className={styles.headerContainer}>
-        {/* Left side */}
-        <div className={styles.headerLeftContainer}>
-          <Link href="/" className={styles.logo}>
+    <>
+      <header
+        className={`${styles.header} ${
+          scrolled ? styles.scrolled : styles.atTop
+        }`}
+      >
+        <div className={styles.headerContainer}>
+          {/* Left side */}
+          <div className={styles.headerLeftContainer}>
+            <Link href="/" className={styles.logo}>
+              <Image
+                src={logo}
+                alt="Aha Logo"
+                priority
+                className={styles.logoImage}
+              />
+            </Link>
+            {/* Large screen Nav */}
+            <nav className={`${styles.nav} ${menuOpen ? styles.open : ""}`}>
+              {navLinks.map((link) => {
+                const isActive = currentPath === link.path;
+                return (
+                  <Link
+                    key={link.name}
+                    href={link.path}
+                    className={`${styles.navItem} ${
+                      isActive ? styles.activeNav : ""
+                    }`}
+                  >
+                    {link.name}
+                  </Link>
+                );
+              })}
+            </nav>
+            {/* Mobile Nav */}
+            <nav
+              className={`${styles.mobileNav} ${menuOpen ? styles.open : ""}`}
+              ref={containerRef}
+              style={{
+                overflowX: "auto",
+                padding: hasOverflow ? "0 14px" : "0",
+                transition: "padding 0.2s ease",
+              }}
+            >
+              {navLinks.slice(0, 4).map((link) => {
+                const isActive = pathname === link.path;
+                return (
+                  <Link
+                    key={link.name}
+                    href={link.path}
+                    className={`${styles.navItem} ${
+                      isActive ? styles.activeNav : ""
+                    }`}
+                  >
+                    {isActive ? (
+                      <div
+                        style={{
+                          backgroundImage: `url(${link.icon.src})`,
+                          backgroundRepeat: "no-repeat",
+                          backgroundPosition: "center",
+                          height: "25px",
+                          width: "25px",
+                        }}
+                      ></div>
+                    ) : (
+                      ""
+                    )}
+
+                    {link.name}
+                  </Link>
+                );
+              })}
+            </nav>
+          </div>
+
+          {/* Right side */}
+          <div className={styles.headerRightContainer}>
             <Image
-              src={logo}
-              alt="Aha Logo"
-              priority
-              className={styles.logoImage}
+              src={search}
+              alt={"Search icon"}
+              className={styles.searchIcon}
             />
-          </Link>
-          {/* Large screen Nav */}
-          <nav className={`${styles.nav} ${menuOpen ? styles.open : ""}`}>
-            {navLinks.map((link) => {
-              const isActive = pathname === link.path;
-              return (
-                <Link
-                  key={link.name}
-                  href={link.path}
-                  className={`${styles.navItem} ${
-                    isActive ? styles.activeNav : ""
-                  }`}
-                >
-                  {link.name}
-                </Link>
-              );
-            })}
-          </nav>
-          {/* Mobile Nav */}
-          <nav
-            className={`${styles.mobileNav} ${menuOpen ? styles.open : ""}`}
-            ref={containerRef}
-            style={{
-              overflowX: "auto",
-              padding: hasOverflow ? "0 14px" : "0",
-              transition: "padding 0.2s ease",
-            }}
-          >
-            {navLinks.map((link) => {
-              const isActive = pathname === link.path;
-              return (
-                <Link
-                  key={link.name}
-                  href={link.path}
-                  className={`${styles.navItem} ${
-                    isActive ? styles.activeNav : ""
-                  }`}
-                >
-                  {isActive ? (
-                    <div
-                      style={{
-                        backgroundImage: `url(${link.icon.src})`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "center",
-                        height: "25px",
-                        width: "25px",
-                      }}
-                    ></div>
-                  ) : (
-                    ""
-                  )}
 
-                  {link.name}
-                </Link>
-              );
-            })}
-          </nav>
-        </div>
+            <select className={styles.languageSelect}>
+              <option value="telugu">Telugu</option>
+              <option value="tamil">Tamil</option>
+            </select>
 
-        {/* Right side */}
-        <div className={styles.headerRightContainer}>
-          <Image
-            src={search}
-            alt={"Search icon"}
-            className={styles.searchIcon}
-          />
-
-          <select className={styles.languageSelect}>
-            <option value="telugu">Telugu</option>
-            <option value="tamil">Tamil</option>
-          </select>
-
-          <Button wrapperClass={styles.subscribeBtn}>Subscribe Now</Button>
-          <div className={styles.signIn}>
-            <div className={styles.avatar}>
-              <Image src={avatar} alt="Profile" width={32} height={32} />
+            <Button wrapperClass={styles.subscribeBtn}>Subscribe Now</Button>
+            <div className={styles.signIn}>
+              <div className={styles.avatar}>
+                <Image src={avatar} alt="Profile" width={32} height={32} />
+              </div>
+              <p>Sign In</p>
             </div>
-            <p>Sign In</p>
           </div>
         </div>
-
-        {/* Mobile menu */}
-        <button
-          className={styles.menuBtn}
-          onClick={() => setMenuOpen((prev) => !prev)}
-        >
-          <Image src={logo} alt="Menu Icon" width={24} height={24} />
-        </button>
-      </div>
-    </header>
+      </header>
+      {/* Bottom Nav */}
+      <BottomNav />
+    </>
   );
 };
 
--- aha-clone/src/utils/Header.ts
+++ aha-clone/src/utils/Header.ts
@@ -3,9 +3,24 @@ import { IHeader } from "@/types/header";
 import allIcon from "../../public/Assets/icons/Header/all-icon.svg";
 import movieIcon from "../../public/Assets/icons/Header/movie-icon.svg";
 import tvIcon from "../../public/Assets/icons/Header/tv-icon.svg";
+import searchIcon from "../../public/Assets/icons/Header/search-icon.svg";
+import homeIcon from "../../public/Assets/icons/Header/home-icon.svg";
+import tickIcon from "../../public/Assets/icons/Header/tick-icon.svg";
+import menuIcon from "../../public/Assets/icons/Header/menu-icon.svg";
+import searchMobileIcon from "../../public/Assets/icons/Header/search-mobile-icon.svg";
+
 export const navLinks: IHeader[] = [
-  { name: "All", path: "/", icon: allIcon },
+  { name: "Home", path: "/", icon: allIcon },
   { name: "Movies", path: "/movies", icon: movieIcon },
   { name: "Shows", path: "/shows", icon: tvIcon },
   { name: "Dance Ikon", path: "/dance-ikon", icon: tvIcon },
+  { name: "Offers", path: "/offers", icon: tvIcon },
+  { name: "My aha", path: "/my-aha", icon: tvIcon },
+];
+
+export const bottomNavLinks = [
+  { name: "Home", path: "/", icon: homeIcon },
+  { name: "My aha", path: "/my-aha", icon: tickIcon },
+  { name: "Search", path: "/search", icon: searchMobileIcon },
+  { name: "Menu", path: "/menu", icon: menuIcon },
 ];
